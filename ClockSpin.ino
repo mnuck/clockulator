e95c7555a2927368d2f5921171fc2ff052f650b9
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2022-02-11 16:03:25 -0800

    don't repeat yourself

--- ClockSpin.ino
+++ ClockSpin.ino
@@ -1,6 +1,7 @@
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <TM1637.h>
 
 const char *ssid     = "*";
 const char *password = "*";
@@ -9,62 +10,47 @@ const int local_offset = -28800; // -8h in sec
 const int utc_offset = 0;
 int dial_offset = 0;
 
-#include <TM1637.h>
-
-//#define CLK1 13
-//#define DIO1 15
-
 #define CLK1 5
 #define DIO1 4
-
 #define CLK2 0
 #define DIO2 2
 
-TM1637 tm1637_local(CLK1,DIO1);
-TM1637 tm1637_utc(CLK2,DIO2);
+TM1637 tm1637_local(CLK1, DIO1);
+TM1637 tm1637_utc(CLK2, DIO2);
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-void setup(){
-  tm1637_local.init();
-  tm1637_local.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
-  tm1637_local.point(POINT_ON);
+void setup_display(TM1637* display) {
+  display->init();
+  display->set(BRIGHT_TYPICAL);
+  display->point(POINT_ON);
+}
 
-  tm1637_utc.init();
-  tm1637_utc.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
-  tm1637_utc.point(POINT_ON);
+void update_display(TM1637* display, int offset) {
+  timeClient.setTimeOffset(offset);
+  int hours = timeClient.getHours();
+  int minutes = timeClient.getMinutes();
+  display->display(0, hours / 10);
+  display->display(1, hours % 10);
+  display->display(2, minutes / 10);
+  display->display(3, minutes % 10);
+}
+
+void setup(){
+  setup_display(&tm1637_local);
+  setup_display(&tm1637_utc);
   
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-
   timeClient.begin();
 }
 
 void loop() {
-  int hours, minutes;
   timeClient.update();
-
-  timeClient.setTimeOffset(local_offset);
-  hours = timeClient.getHours();
-  minutes = timeClient.getMinutes();
-
-  tm1637_local.display(0, hours / 10);
-  tm1637_local.display(1, hours % 10);
-  tm1637_local.display(2, minutes / 10);
-  tm1637_local.display(3, minutes % 10);
-
-  timeClient.setTimeOffset(utc_offset);
-  hours = timeClient.getHours();
-  minutes = timeClient.getMinutes();
-
-  tm1637_utc.display(0, hours / 10);
-  tm1637_utc.display(1, hours % 10);
-  tm1637_utc.display(2, minutes / 10);
-  tm1637_utc.display(3, minutes % 10);
-
+  update_display(&tm1637_local, local_offset + dial_offset);
+  update_display(&tm1637_utc, utc_offset + dial_offset);
   delay(1000);
 }

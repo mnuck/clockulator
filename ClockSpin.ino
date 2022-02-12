f98037b6f5f304b39a1c329875601dbbb20fc65c
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2022-02-11 22:56:42 -0800

    Add rotary and button

--- ClockSpin.ino
+++ ClockSpin.ino
@@ -2,24 +2,31 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <TM1637.h>
+#include <ESPRotary.h>
+#include <Button2.h>
 
 const char *ssid     = "*";
 const char *password = "*";
 
+#define CLK2 0
+#define DIO2 2
+
+#define ROTARY_PIN1 14
+#define ROTARY_PIN2 12
+#define ROTARY_BUTTON 13
+
 const int local_offset = -28800; // -8h in sec
 const int utc_offset = 0;
 int dial_offset = 0;
-
-#define CLK1 5
-#define DIO1 4
-#define CLK2 0
-#define DIO2 2
 
 TM1637 tm1637_local(CLK1, DIO1);
 TM1637 tm1637_utc(CLK2, DIO2);
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+ESPRotary rotary(ROTARY_PIN1, ROTARY_PIN2);
+Button2 button;
 
 void setup_display(TM1637* display) {
   display->init();
@@ -37,14 +44,27 @@ void update_display(TM1637* display, int offset) {
   display->display(3, minutes % 10);
 }
 
+void decrementOffset(ESPRotary& r) {
+  dial_offset += 600;
+}
+
+void incrementOffset(ESPRotary& r) {
+  dial_offset -= 600;
+}
+
+void resetOffset(Button2& b) {
+  dial_offset = 0;
+}
+
 void setup(){
   setup_display(&tm1637_local);
   setup_display(&tm1637_utc);
-  
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+
+  rotary.setLeftRotationHandler(decrementOffset);
+  rotary.setRightRotationHandler(incrementOffset);
+
+  button.begin(ROTARY_BUTTON);
+  button.setTapHandler(resetOffset);
   timeClient.begin();
 }
 
@@ -52,5 +72,6 @@ void loop() {
   timeClient.update();
   update_display(&tm1637_local, local_offset + dial_offset);
   update_display(&tm1637_utc, utc_offset + dial_offset);
-  delay(1000);
+  rotary.loop();
+  button.loop();
 }
